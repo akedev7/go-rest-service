38aaab7fc50caf41d82080d2dfb8e27b988ab4ee
ake.tan <ake.tan@ascendcorp.com>
2022-01-03 10:54:16 +0700

    Include persistance for SQLLite using GORM for ORM

--- main.go
+++ main.go
@@ -4,9 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 func main() {
+
+	var err error
+	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Book{})
+
 	r := gin.New()
 	r.GET("/books", listBooksHandler)
 	r.POST("/books", createBookHandler)
