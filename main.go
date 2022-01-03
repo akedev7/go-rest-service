f2626cbeb83ad43041340ed6557badcca7125f2c
ake.tan <ake.tan@ascendcorp.com>
2022-01-03 10:27:29 +0700

    Init commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author`
+}
+
+var books = []Book{
+	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
+	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
+	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+}
+
+func main() {
+
+	fmt.Println("test")
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	r.GET("/books", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, books)
+	})
+
+	r.POST("/books", func(c *gin.Context) {
+
+		var book Book
+		if err := c.ShouldBindJSON(&book); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		books = append(books, book)
+		c.JSON(http.StatusCreated, book)
+
+	})
+
+	r.DELETE("/books/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		for i, a := range books {
+			if a.ID == id {
+				books = append(books[:i], books[i+1:]...)
+				break
+			}
+		}
+		c.Status(http.StatusNoContent)
+
+	})
+
+	r.Run()
+}
